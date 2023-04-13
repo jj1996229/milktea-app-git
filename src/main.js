532d7dce717499614e0f282d18e09fb41fb0dc29
cxj <598820867@qq.com>
2023-04-13 15:23:53 +0800

    更新main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vant from 'vant'
-
 import 'vant/lib/index.css';
-
-
-// 1. 引入你需要的组件
-import vant from 'vant';
-// 2. 引入组件样式
-import 'vant/lib/index.css';
-
 import store from'./store';
 
 
@@ -19,9 +11,5 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vant)
-<<<<<<< HEAD
-
-=======
 app.use(store)
->>>>>>> d6690e5a82d6cc8e888eb65df80ad12c4bb383e2
 app.mount('#app')
