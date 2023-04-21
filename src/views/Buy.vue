ec2bce5141b89f5094c5b71276af304f97f198d9
cxj <598820867@qq.com>
2023-04-21 10:53:32 +0800

    修改了点单页面

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
-        buy
+
     </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        
+    }
 </script>
 
 <style lang="scss" scoped>
+
 </style>
